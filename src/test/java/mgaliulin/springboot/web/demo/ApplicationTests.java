471ab88cf7454012881c0e8316df696c26456f9c
maratgaliulin <egomarat@gmail.com>
2024-12-03 00:33:18 +1100

    initial commit

---
+++ src/test/java/mgaliulin/springboot/web/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mgaliulin.springboot.web.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
