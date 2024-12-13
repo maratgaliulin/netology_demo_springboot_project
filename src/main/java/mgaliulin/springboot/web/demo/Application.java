f47a03ea54ad9f7f987998f51cc211aa13fb3423
maratgaliulin <egomarat@gmail.com>
2024-12-13 13:43:19 +1100

    tomcat server preparation

--- src/main/java/mgaliulin/springboot/web/demo/Application.java
+++ src/main/java/mgaliulin/springboot/web/demo/Application.java
@@ -2,6 +2,7 @@ package mgaliulin.springboot.web.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application {
